66fd0d187c938009eb17c656d1e6bc7f4f6f6d85
hadasvolk <hadasvol@pm.me>
2022-07-20 09:36:20 +0000

    fixed epsilon

--- hoobGender.py
+++ hoobGender.py
@@ -200,7 +200,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--bam', help='Path to BAM file', required=True, type=str)
     parser.add_argument('-s', '--sample', help='Sample name', required=True, type=str)
     parser.add_argument('-f', '--fractions', help='Path to fractions pickle file', required=True, type=str)
-    parser.add_argument('-e', '--epsilon', help='Epsilon for No Call region', required=False, type=float, default=0.5)
+    parser.add_argument('-e', '--epsilon', help='Epsilon for No Call region', required=False, type=float, default=0.05)
     
     parser.add_argument('-o', '--out', help='Output directory', required=False, default='./hoobGender', type=str)
     parser.add_argument('-log', '--log', default="info", help=("Provide logging level. Example --log warning', default='info'"))
